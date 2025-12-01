ad1c16f3be9191fdcaaf3e05633394eee7687a60
John Wilson <johnny@Johns-MBP.cable.virginm.net>
2025-12-01 20:59:34 +0000

    Tidy

--- webbot.sh
+++ webbot.sh
@@ -3,6 +3,7 @@ hostname=$(hostname)
 source /opt/playwright-env/bin/activate
 touch /root/.Xauthority
 while [[ true ]]; do
+ echo "*** WARNING - CHECK YOU HAVE vhclient running if using a remote FIDO device ***"
  python3 webbot.py $@
  #python3 message.py "From host `hostname`. Webbot terminated ...`date`"
  #./announce.sh "We have logged out of $hostname"

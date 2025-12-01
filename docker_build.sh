bf99d1aa9b3fc23b177434c8cda57792209ec9c1
John Wilson <johnny@Johns-MBP.cable.virginm.net>
2025-12-01 17:46:40 +0000

    Update in build scripts to take --no-cache as a paramter

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,8 @@
 chmod +x *.sh
-docker build -f Dockerfile.freeze -t webbot .
 
+# Capture all arguments
+ARGS="$@"
+
+# Run docker build with the Dockerfile.freeze and any args passed
+docker build $ARGS -f Dockerfile.freeze -t webbot .
 
